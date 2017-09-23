ea4dd8c3b23aa2f43aa78bdebbf05bff72501109
Unknown <eric.schneider@rutgers.edu>
2017-09-23 12:59:32 -0400

    Basic Front end layout applied

--- js/normalScript.js
+++ js/normalScript.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
-  $.post( "php/solrcheck.php", {rows: 20, rowStart:0, postType:"normal",normalData:"mod,help,OP,size",searchBy:"all"}, function( data ) {
-    $( "#text-container" ).html( data );
+  $.post( "php/solrcheck.php", {rows: 1, rowStart:0, postType:"normal",normalData:"mod,help,OP,size",searchBy:"all"}, function( data ) {
+    //$( "#text-container" ).html( data );
   });
 
 });
